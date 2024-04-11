1abf54ac7fe0e8cae8e428657fa9ed20514f98b8
Irson <jiordesta@gmail.com>
2024-04-11 10:30:01 +0800

    .

--- src/redux/reducers/blocksSlice.ts
+++ src/redux/reducers/blocksSlice.ts
@@ -3,10 +3,12 @@ import { Block, BlockId, BlockType, createNewBlock } from "../../utils/blocks";
 // @ts-ignore
 import { v4 } from "uuid";
 
+// Define the shape of the state
 interface State {
   block: Block;
 }
 
+// Set the initial state
 const initialState: State = {
   block: {
     type: "Column",
@@ -16,6 +18,7 @@ const initialState: State = {
   } as Block, // Initialize with an empty block or appropriate default values
 };
 
+// Recursive function to find a block by ID and add a new block to its children
 const findBlockAndAddNewBlock = (
   block: Block,
   type: BlockType,
@@ -24,10 +27,10 @@ const findBlockAndAddNewBlock = (
   const newBlock: Block = createNewBlock(type);
 
   if (block.id === id) {
-    ///if the block is equla to the id push the new block to its children
+    // If the block matches the given ID, push the new block to its children
     block.children?.push(newBlock);
   } else {
-    ///recursively find the block given the id
+    // Recursively search for the block in its children
     if (block.children && block.children.length > 0) {
       block.children.forEach((block) => {
         findBlockAndAddNewBlock(block, type, id);
@@ -37,6 +40,7 @@ const findBlockAndAddNewBlock = (
   return block;
 };
 
+// Recursive function to find a block by ID and delete it
 const findBlockAndDelete = (block: Block, id: BlockId): Block => {
   if (block.children && block.children.length > 0) {
     block.children.map((childBlock) => {
@@ -52,6 +56,7 @@ const findBlockAndDelete = (block: Block, id: BlockId): Block => {
   return block;
 };
 
+// Function to add styles to a block by ID
 const addBlockStyling = (block: Block, id: BlockId, styles: Object): Block => {
   if (block.id === id) {
     block.options = { ...block.options, ...styles };
@@ -64,6 +69,7 @@ const addBlockStyling = (block: Block, id: BlockId, styles: Object): Block => {
   return block;
 };
 
+// Function to add data to a block by ID
 const addBlockData = (block: Block, id: BlockId, data: String): Block => {
   if (block.id === id) {
     if (block.data) {
@@ -80,19 +86,23 @@ const addBlockData = (block: Block, id: BlockId, data: String): Block => {
   return block;
 };
 
+// Create a Redux slice
 const blocksSlice = createSlice({
   name: "block",
   initialState,
   reducers: {
+    // Action to add a block
     add_block: (state, action) => {
       const { type, id, size } = action.payload;
       for (let i = 0; i < size; i++) {
         state.block = findBlockAndAddNewBlock(state.block, type, id);
       }
     },
+    // Action to delete a block
     delete_block: (state, action) => {
       state.block = findBlockAndDelete(state.block, action.payload);
     },
+    // Action to add styles to a block
     add_style: (state, action) => {
       state.block = addBlockStyling(
         state.block,
@@ -100,6 +110,7 @@ const blocksSlice = createSlice({
         action.payload.styles
       );
     },
+    // Action to add data to a block
     add_data: (state, action) => {
       state.block = addBlockData(
         state.block,
@@ -110,6 +121,7 @@ const blocksSlice = createSlice({
   },
 });
 
+// Export actions and reducer
 export const { add_block, delete_block, add_style, add_data } =
   blocksSlice.actions;
 export default blocksSlice.reducer;
